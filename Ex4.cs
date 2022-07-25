abe0ec7aecdb1ecd0e088302f656248c9010cbe0
hungxt232467 <104129840+hungxt232467@users.noreply.github.com>
2022-07-25 17:04:33 +0700

    Add files via upload

--- Ex4.cs
+++ Ex4.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class MianClass
+{
+    public static int AnExceptionFunction(int value)
+    {
+        if (value == 0) throw new DivideByZeroException();
+        int x = 20;
+        return x;
+    }
+    public static void Main()
+    {
+        int value = 0;
+        try
+        {
+            value = AnExceptionFunction(10);
+            Console.WriteLine("Value = {0}", value);
+            AnExceptionFunction(0);
+            Console.WriteLine("Value = {0}", value);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Caught an exception {0}.Continuing", e);
+
+        }
+        Console.WriteLine("Done");
+    }
+}
